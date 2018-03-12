ce44a52c58151f40029de9bc79b3e78a566681fb
ZHU Fangda <zhufangda@gmail.com>
2018-03-12 21:04:23 +0100

    Initialisation du Partie android

---
+++ CarteResto_Android/app/src/main/java/com/carteresto/igr230/carteresto/MenuDetail/MenuDetailActivity.java
@@ -0,0 +1,15 @@
+package com.carteresto.igr230.carteresto.MenuDetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.carteresto.igr230.carteresto.R;
+
+public class MenuDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_detail);
+    }
+}
---
+++ CarteResto_Android/app/src/main/java/com/carteresto/igr230/carteresto/MenuPrincipale/MenuPrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.carteresto.igr230.carteresto.MenuPrincipale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.carteresto.igr230.carteresto.R;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
